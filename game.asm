48b2d92a4add1ec895c0e5dc88c69154f732f2f6
Oláh Levente <levente@stud.uni-obuda.hu>
2024-10-14 07:42:03 +0200

    update game.asm

--- game.asm
+++ game.asm
@@ -2,23 +2,22 @@
 Code	Segment
 	assume CS:Code, DS:Data, SS:Stack
 
-UNDERTALE db "Long ago, two races$"
-UNDERTALE2 db "ruled over Earth:$"
-UNDERTALE3 db "HUMANS and MONSTERS.$"
+TITLE1 DB "Is Sysiphus happy?$"
+TITLE2 DB "He isn't pushing boulders, but the boulders pushing him$"
 
 PLAYER DB "P$"
 playerX DB 1
-playerY DB 15
+playerY DB 12
 
 OBSTACLE DB "O$"
-obstacleX DB 10
-obstacleY DB 17
+; obstacleX DB 50
+; obstacleY DB 10
 
-PLAY_AREA DB "________________________________________________________________________________"
-playarea2 DB ,10,13,10,13,10,13,10,13,10,13,10,13,10,13,10,13,10,13,10,13,10,13,10,13,10,13,
-playarea3 DB "_______________________________________________________________________________$"
+PLAY_AREA DB "________________________________________________________________________________$"
+topBorder DB 5
+bottomBorder DB 19
 
-VEGE DB "THE END! :($"
+VEGE DB "Sysiphus died! :($"
 
 Start:
 	mov	ax, Code
@@ -26,13 +25,15 @@ Start:
 
 MainLoop:
 	CALL ClearScreen
-
 	CALL DrawTitle
 	CALL DrawPlayArea
+	Call DrawObstacles
+	; CALL MoveObstacles
+
 	CALL DrawPlayer
-	; CALL DrawObstacle
 
 	CALL MovePlayer
+	CALL CheckCollision
 
 	JMP MainLoop
 
@@ -89,6 +90,25 @@ MOZGAS_JOBBRA:
 MovePlayerEnd:
 	RET
 
+CheckCollision:
+
+	mov ah, 02h ;Set cursor position
+	mov bh, 0
+	mov dh, [playerY]
+	mov dl, [playerX]
+	int 10h
+
+	mov ah, 08h ;Read character at cursor
+    xor bh, bh       
+    int 10h
+
+	cmp al, [OBSTACLE]; character is obstacle
+	JZ JATEK_VEGE
+
+	RET
+
+
+
 JATEK_VEGE:
 	CALL ClearScreen
 
@@ -115,41 +135,43 @@ DrawTitle:
 	INT 10H
 
 	MOV AH,9
-	MOV DX, OFFSET UNDERTALE
+	MOV DX, OFFSET TITLE1
 	INT 21H
 
 	MOV AH,2 ; SET CURSOR POSITION
 	XOR BH,BH
 	MOV DH,2
-	MOV DL,30
+	MOV DL,20
 	INT 10H
 
 	MOV AH,9
-	MOV DX, OFFSET UNDERTALE2
+	MOV DX, OFFSET TITLE2
 	INT 21H
 
-	MOV AH,2 ; SET CURSOR POSITION
-	XOR BH,BH
-	MOV DH,3
-	MOV DL,30
-	INT 10H
-
-	MOV AH,9
-	MOV DX, OFFSET UNDERTALE3
-	INT 21H
 	RET
 
 
 DrawPLayArea:
 	MOV AH,2 ; SET CURSOR POSITION
 	XOR BH,BH
-	MOV DH,5
+	MOV DH, [topBorder]
 	MOV DL,0
 	INT 10H
 
 	MOV AH,9
 	MOV DX, OFFSET PLAY_AREA
 	INT 21H
+
+	MOV AH,2 ; SET CURSOR POSITION
+	XOR BH,BH
+	MOV DH, [bottomBorder]
+	MOV DL,0
+	INT 10H
+
+	MOV AH,9
+	MOV DX, OFFSET PLAY_AREA
+	INT 21H
+
 	RET
 
 DrawPlayer:
@@ -164,17 +186,102 @@ DrawPlayer:
 	INT 21H
 	RET
 
-DrawObstacle:
+
+DrawObstacles:
 
 	MOV AH,2 ; SET CURSOR POSITION
 	XOR BH,BH
-	MOV DH,[obstacleY]
-	MOV DL,[obstacleX]
+	MOV DH, 8
+	MOV DL, 35
 	INT 10H
+	CALL DrawObstacle
+
+	MOV AH,2 ; SET CURSOR POSITION
+	XOR BH,BH
+	MOV DH, 15
+	MOV DL, 50
+	INT 10H
+	CALL DrawObstacle
+
+	MOV AH,2 ; SET CURSOR POSITION
+	XOR BH,BH
+	MOV DH, 7
+	MOV DL, 20
+	INT 10H
+	CALL DrawObstacle
+
+	MOV AH,2 ; SET CURSOR POSITION
+	XOR BH,BH
+	MOV DH, 17
+	MOV DL, 10
+	INT 10H
+	CALL DrawObstacle
+
+	RET
+
+
+MoveObstacles:
+	; MOV DH, 10
+	; MOV DL, 40
+	; XOR BH,BH
+	; CALL MoveObstacle
+
+	; MOV DH, 11
+	; MOV DL, 41
+	; XOR BH,BH
+	; CALL MoveObstacle
+
+	; MOV DH, 12
+	; MOV DL, 42
+	; XOR BH,BH
+	; CALL MoveObstacle
+
+	MOV DH, 13
+	MOV DL, 43
+	XOR BH,BH
+	CALL MoveObstacle
+
+	RET
+
+MoveObstacle:
+	MOV AH, 02h ; Set cursor position
+	INT 10h
+	CALL DrawObstacle
+	CALL wait_1_sec
+	; Erase the current character by printing a space
+    mov ah, 09h        ; BIOS function 09h - Write character and attribute
+    mov al, ' '        ; Print space to "erase" the character
+    mov bl, 07h        ; White on black attribute
+    mov cx, 1          ; Print one space
+    int 10h            ; Call BIOS to erase character
+
+    ; Move left by decreasing the column (dl)
+    dec dl             ; Decrement column to move left
+    cmp dl, 0          ; Check if we've reached the leftmost column
+    jl done            ; If dl < 0, end the program
+	jmp MoveObstacle
+
+	done:
+		RET
+
+wait_1_sec:
+	mov ah, 00h        ; Get system time
+	int 1Ah            ; BIOS timer interrupt
+	mov dx, bx         ; Store current time
+	add dx, 18         ; Add 18 ticks (~1 second delay)
+wait_loop:
+	mov ah, 00h        ; Get system time again
+	int 1Ah            ; BIOS timer interrupt
+	cmp bx, dx         ; Compare current tick count with target
+	jb wait_loop       ; If not reached target, keep waiting
+RET
+
+DrawObstacle:
 	MOV AH,9
 	MOV DX, OFFSET OBSTACLE
 	INT 21H
 	RET
+
 
 Program_Vege:
 	mov	ax, 4c00h
