87a621ff4c595f97cc35ba816fd2e76e789f472e
Oláh Levente <levente@stud.uni-obuda.hu>
2024-10-21 20:24:32 +0200

    updated game

--- game.asm
+++ game.asm
@@ -10,32 +10,55 @@ playerX DB 1
 playerY DB 12
 
 OBSTACLE DB "O$"
-; obstacleX DB 50
-; obstacleY DB 10
+obstacleX DB 50
+obstacleY DB 10
 
 PLAY_AREA DB "________________________________________________________________________________$"
-topBorder DB 5
-bottomBorder DB 19
+bottomBorder DB 5
+topBorder DB 19
 
 VEGE DB "Sysiphus died! :($"
+WIN DB "Sysiphus is finally happy!$"
+
+SCORE_STR DB "Score: "
+SCORE_HUNDREDS DB "0"
+SCORE_TENS DB "0"
+SCORE_ONES DB "0"
+SCORE_END DB "$"
+
+RETRY DB "Try again? [y/n]$"
+
+TICKS DW 0
+TICKS2 DW 0
 
 Start:
 	mov	ax, Code
 	mov	DS, AX
 
 MainLoop:
+	CALL GetTicks
 	CALL ClearScreen
 	CALL DrawTitle
 	CALL DrawPlayArea
-	Call DrawObstacles
-	; CALL MoveObstacles
+	CALL DrawScore
+
+	CALL MoveObstacle
+	CALL DrawObstacle
 
 	CALL DrawPlayer
 
 	CALL MovePlayer
 	CALL CheckCollision
-
+	CALL IncrementScore
+	; CALL OneSecDelay
 	JMP MainLoop
+
+GetTicks:
+	MOV AH, 0
+	INT 1AH
+	MOV [TICKS], CX
+	MOV [TICKS2], DX
+	RET
 
 ClearScreen:
 	MOV AX, 3
@@ -43,6 +66,10 @@ ClearScreen:
 	RET
 
 MovePlayer:
+	; MOV AH, 01H
+	; INT 16H
+	; JZ MovePlayerEnd
+
 	XOR AH, AH
 	INT 16H
 
@@ -57,61 +84,69 @@ MovePlayer:
 	JE MOZGAS_JOBBRA
 
 	CMP AL, 1bh ; ESC
-	JE JATEK_VEGE
+	JE JumpToEnd
 
-	JMP MovePlayer
-	
 MOZGAS_FEL:
-	MOV AL, [playerY] 
-	DEC AL
-	MOV [playerY], AL
+	DEC [playerY]
+
+	CALL CheckBorders
+	CMP AL, '_'
+	JE MOZGAS_LE
+
 	JMP MovePlayerEnd
 
 
 MOZGAS_LE:
-	MOV AL, [playerY] 
-	INC AL
-	MOV [playerY], AL
+	INC [playerY]
+
+	CALL CheckBorders
+	CMP AL, '_'
+	JE MOZGAS_FEL
 
 	JMP MovePlayerEnd
 
 MOZGAS_BALRA:
-	MOV AL, [playerX] 
-	DEC AL
-	MOV [playerX], AL
+	DEC [playerX]
+
+	CMP [PLAYERX], -1
+	JE MOZGAS_JOBBRA
+
 	JMP MovePlayerEnd
 
 MOZGAS_JOBBRA:
-	MOV AL, [playerX] 
-	INC AL
-	MOV [playerX], AL
+	INC [playerX]
+
+	CMP [PLAYERX], 80
+	JE MOZGAS_BALRA
 	JMP MovePlayerEnd
 
 MovePlayerEnd:
 	RET
-
-CheckCollision:
-
-	mov ah, 02h ;Set cursor position
-	mov bh, 0
+CheckBorders:
+	mov ah, 02h ;Set cursor
+	xor bh, bh
 	mov dh, [playerY]
 	mov dl, [playerX]
 	int 10h
 
-	mov ah, 08h ;Read character at cursor
-    xor bh, bh       
+	mov ah, 08h ;Read character
+    xor bh, bh
     int 10h
 
-	cmp al, [OBSTACLE]; character is obstacle
-	JZ JATEK_VEGE
-
 	RET
-
-
+JumpToEnd:
+	JMP JATEK_VEGE
+ResetGame:
+	MOV [playerX], 1
+	MOV [playerY], 12
+	MOV [obstacleX], 50
+	MOV [obstacleY], 10
+	MOV [SCORE_TENS], "0"
+	MOV [SCORE_ONES], "0"
+	JMP MainLoop
 
 JATEK_VEGE:
 	CALL ClearScreen
-
 	MOV AH,2 ; SET CURSOR POSITION
 	XOR BH,BH
 	MOV DH,10
@@ -122,11 +157,48 @@ JATEK_VEGE:
 	MOV DX, OFFSET VEGE
 	INT 21H
 
+	MOV AH,2 ; SET CURSOR POSITION
+	XOR BH,BH
+	MOV DH,11
+	MOV DL,34
+	INT 10H
+	MOV AH,9
+	MOV DX, OFFSET SCORE_STR
+	INT 21H
+
+	MOV AH,2 ; SET CURSOR POSITION
+	XOR BH,BH
+	MOV DH,13
+	MOV DL,30
+	INT 10H
+	MOV AH,9
+	MOV DX, OFFSET RETRY
+	INT 21H
+
 	XOR AH, AH
 	INT 16H
 
+	CMP AL, 'y'
+	JE ResetGame
+
 	jmp Program_Vege
 
+CheckCollision:
+
+	mov ah, 02h ;Set cursor position
+	xor bh, bh
+	mov dh, [playerY]
+	mov dl, [playerX]
+	int 10h
+
+	mov ah, 08h ;Read character at cursor
+    xor bh, bh
+    int 10h
+
+	cmp al, [OBSTACLE]; character is obstacle
+	JZ JATEK_VEGE
+
+	RET
 DrawTitle:
 	MOV AH,2 ; SET CURSOR POSITION
 	XOR BH,BH
@@ -141,7 +213,7 @@ DrawTitle:
 	MOV AH,2 ; SET CURSOR POSITION
 	XOR BH,BH
 	MOV DH,2
-	MOV DL,20
+	MOV DL,15
 	INT 10H
 
 	MOV AH,9
@@ -174,6 +246,17 @@ DrawPLayArea:
 
 	RET
 
+DrawScore:
+	MOV AH,2 ; SET CURSOR POSITION
+	XOR BH,BH
+	MOV DH, [topBorder] - 1
+	MOV DL, 68
+	INT 10h
+
+	MOV AH,9
+	MOV DX, OFFSET SCORE_STR
+	INT 21H
+
 DrawPlayer:
 	MOV AH,2 ; SET CURSOR POSITION
 	XOR BH,BH
@@ -186,102 +269,86 @@ DrawPlayer:
 	INT 21H
 	RET
 
-
-DrawObstacles:
-
-	MOV AH,2 ; SET CURSOR POSITION
-	XOR BH,BH
-	MOV DH, 8
-	MOV DL, 35
-	INT 10H
-	CALL DrawObstacle
-
-	MOV AH,2 ; SET CURSOR POSITION
-	XOR BH,BH
-	MOV DH, 15
-	MOV DL, 50
-	INT 10H
-	CALL DrawObstacle
-
-	MOV AH,2 ; SET CURSOR POSITION
-	XOR BH,BH
-	MOV DH, 7
-	MOV DL, 20
-	INT 10H
-	CALL DrawObstacle
-
-	MOV AH,2 ; SET CURSOR POSITION
-	XOR BH,BH
-	MOV DH, 17
-	MOV DL, 10
-	INT 10H
-	CALL DrawObstacle
-
-	RET
-
-
-MoveObstacles:
-	; MOV DH, 10
-	; MOV DL, 40
-	; XOR BH,BH
-	; CALL MoveObstacle
-
-	; MOV DH, 11
-	; MOV DL, 41
-	; XOR BH,BH
-	; CALL MoveObstacle
-
-	; MOV DH, 12
-	; MOV DL, 42
-	; XOR BH,BH
-	; CALL MoveObstacle
-
-	MOV DH, 13
-	MOV DL, 43
-	XOR BH,BH
-	CALL MoveObstacle
-
-	RET
-
 MoveObstacle:
-	MOV AH, 02h ; Set cursor position
-	INT 10h
-	CALL DrawObstacle
-	CALL wait_1_sec
-	; Erase the current character by printing a space
-    mov ah, 09h        ; BIOS function 09h - Write character and attribute
-    mov al, ' '        ; Print space to "erase" the character
-    mov bl, 07h        ; White on black attribute
-    mov cx, 1          ; Print one space
-    int 10h            ; Call BIOS to erase character
+	MOV AH,2 ; SET CURSOR POSITION
+	XOR BH,BH
+	MOV DH,[obstacleY]
+	MOV DL,[obstacleX]
+	INT 10H
 
-    ; Move left by decreasing the column (dl)
-    dec dl             ; Decrement column to move left
-    cmp dl, 0          ; Check if we've reached the leftmost column
-    jl done            ; If dl < 0, end the program
-	jmp MoveObstacle
+	DEC [obstacleX]
+	CMP [obstacleX], -1
+	JNE M_O_END
 
-	done:
-		RET
+	MOV [obstacleX], 80
 
-wait_1_sec:
-	mov ah, 00h        ; Get system time
-	int 1Ah            ; BIOS timer interrupt
-	mov dx, bx         ; Store current time
-	add dx, 18         ; Add 18 ticks (~1 second delay)
-wait_loop:
-	mov ah, 00h        ; Get system time again
-	int 1Ah            ; BIOS timer interrupt
-	cmp bx, dx         ; Compare current tick count with target
-	jb wait_loop       ; If not reached target, keep waiting
-RET
-
+	M_O_END:
+	RET
 DrawObstacle:
 	MOV AH,9
 	MOV DX, OFFSET OBSTACLE
 	INT 21H
 	RET
 
+IncrementScore:
+	
+	CMP [SCORE_HUNDREDS], "9"
+	JNE S_S_continue
+	CMP [SCORE_TENS], "9"
+	JNE S_S_continue
+	CMP [SCORE_ONES], "9"
+	JNE S_S_continue
+	;mindhárom 999
+	;JMP WinScene
+	
+	S_S_continue:
+	CMP [SCORE_TENS], "9"
+	JNE NoHundreds
+	CMP [SCORE_ONES], "9"
+	JE IncrementHundred
+
+	NoHundreds:
+	CMP [SCORE_ONES], "9"
+	JNE NoTens
+	INC [SCORE_TENS]
+	MOV [SCORE_ONES], "0"
+	JMP S_S_End
+
+	NoTens:
+	INC [SCORE_ONES]
+	JMP S_S_End
+
+	IncrementHundred:
+	INC [SCORE_HUNDREDS]
+	MOV [SCORE_TENS], "0"
+	MOV [SCORE_ONES], "0"
+	JMP S_S_End
+
+	S_S_End:
+	RET
+
+OneSecDelay:
+	; MOV AH, 00H
+	; INT 1AH
+	; CMP DX, [TICKS2] + 18
+	; JB OneSecDelay
+	; RET
+
+WinScene:
+	CALL ClearScreen
+
+	MOV AH,2 ; SET CURSOR POSITION
+	XOR BH,BH
+	MOV DH,12
+	MOV DL,27
+	INT 10H
+
+	MOV AH,9
+	MOV DX, OFFSET WIN
+	INT 21h
+
+	XOR AH, AH
+	INT 16H 
 
 Program_Vege:
 	mov	ax, 4c00h
